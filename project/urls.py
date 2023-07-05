1b9ea11171e70775d834b760f66f24329d0a1e56
Wanderson Rossetti <pastarossetti@gmail.com>
2023-07-05 09:15:11 -0300

    create urls request response

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def my_view(request):
+    print('posso fazer outras coisas')
+    return HttpResponse('Uma mensagem para alguém')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', my_view),
 ]
