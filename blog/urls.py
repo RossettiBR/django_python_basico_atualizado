375d3deeb35b525d718abe1eb5cb6135c96887f9
Wanderson Rossetti <pastarossetti@gmail.com>
2023-07-06 12:04:50 -0300

    update

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.blog, name='blog'),
-    path('post/<int:id>/', views.post, name='post'),
+    path('', views.blog, name='home'),
+    path('<int:id>/', views.post, name='post'),
     path('exemplo/', views.exemplo, name='exemplo')
 ]
